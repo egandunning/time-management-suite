730a4160fc0b4a2436918ddad2cf3599ed47660f
egandunning <dunninea@plu.edu>
2017-11-15 20:11:02 -0500

    more descriptive info page

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -14,7 +15,8 @@ public class Main extends Application {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("/main_window.fxml"));
 			primaryStage.setScene(new Scene(root, 800, 600));
-			
+			primaryStage.setTitle("Time management suite");
+			primaryStage.getIcons().add(new Image("icon.png"));
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
