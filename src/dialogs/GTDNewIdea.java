f503491662a9f60b877e7eca50a5fb807c100935
egandunning <dunninea@plu.edu>
2017-12-06 18:18:04 -0500

    changed type of gtd dialog to alerttype.none

--- src/dialogs/GTDNewIdea.java
+++ src/dialogs/GTDNewIdea.java
@@ -10,11 +10,12 @@ import javafx.scene.layout.GridPane;
 
 public class GTDNewIdea extends Alert {
 
+	private String idea;
+	private String list;
+	
 	public GTDNewIdea() {
-		super(Alert.AlertType.CONFIRMATION);
+		super(Alert.AlertType.NONE);
 		setTitle("Record New Idea");
-		setHeaderText(""); //get that ugly header outta here!
-		setGraphic(new Label()); //same for the icon!
 		GridPane gp = new GridPane();
 		gp.setHgap(10);
 		gp.setVgap(10);
@@ -23,13 +24,26 @@ public class GTDNewIdea extends Alert {
 				FXCollections.observableList(
 						GTDLists.getInstance().getLists()));
 		
+		//add elements to layout
 		gp.add(new Label("Pick a list to add to:"), 0, 0);
 		gp.add(list, 1, 0);
 		
 		gp.add(new Label("The idea:"), 0, 1);
 		gp.add(new TextField(), 1, 1);
 		
+		//add event listeners
+		getDialogPane().getScene().getWindow()
+			.setOnCloseRequest(event -> hide());
+		
+		//add layout to pane
 		getDialogPane().setContent(gp);
 	}
 
+	public String getIdea() {
+		return idea;
+	}
+
+	public String getList() {
+		return list;
+	}
 }
