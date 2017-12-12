23dc60ff81c0e07f9fd2e90ea99dffb2a2c459ea
egandunning <dunninea@plu.edu>
2017-12-12 08:05:13 -0500

    added drag-n-drop functionality for gtd items

--- src/controllers/GTDController.java
+++ src/controllers/GTDController.java
@@ -1,8 +1,15 @@
 package controllers;
 
 import dialogs.GTDNewIdea;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.util.Pair;
@@ -18,6 +25,7 @@ public class GTDController {
 	private Button newIdea;
 	@FXML
 	private VBox inList;
+	Text inListDrop;
 	@FXML
 	private VBox nextActionsList;
 	@FXML
@@ -30,6 +38,96 @@ public class GTDController {
 	@FXML
 	protected void initialize() {
 		System.out.println("In initialize (GTD)");
+		
+		//drag and drop listeners
+		inList.setOnDragOver(new DragOverHandler<DragEvent>());
+		inList.setOnDragDropped(new DragDroppedHandler<DragEvent>(inList));
+		
+		nextActionsList.setOnDragOver(new DragOverHandler<DragEvent>());
+		nextActionsList.setOnDragDropped(new DragDroppedHandler<DragEvent>(nextActionsList));
+		
+		waitingForList.setOnDragOver(new DragOverHandler<DragEvent>());
+		waitingForList.setOnDragDropped(new DragDroppedHandler<DragEvent>(waitingForList));
+		
+		projectsList.setOnDragOver(new DragOverHandler<DragEvent>());
+		projectsList.setOnDragDropped(new DragDroppedHandler<DragEvent>(projectsList));
+		
+		somedayList.setOnDragOver(new DragOverHandler<DragEvent>());
+		somedayList.setOnDragDropped(new DragDroppedHandler<DragEvent>(somedayList));
+	}
+	/**
+	 * Class for handling drag over events - needed for multiple layouts
+	 * @param <T> the event type - should be DragEvent
+	 */
+	private class DragOverHandler<T extends Event> implements EventHandler<T> {
+		@Override
+		public void handle(Event event) {
+			((DragEvent)event).acceptTransferModes(TransferMode.COPY_OR_MOVE);
+        }	
+	}
+	
+	/**
+	 * Class for handling drop events - needed to move items from one layout
+	 * to another
+	 * @param <T> the event type - should be DragEvent
+	 */
+	private class DragDroppedHandler<T extends Event> implements EventHandler<T> {
+
+		VBox layout;
+		
+		public DragDroppedHandler(VBox layout) {
+			this.layout = layout;
+		}
+		
+		@Override
+		public void handle(Event event) {
+			layout.getChildren().add(new Text(((DragEvent)event).getDragboard().getString()));
+			
+			Text lastElement = (Text) layout.getChildren().get(layout.getChildren().size()-1);
+			lastElement.setOnDragDetected(new DragHandler<MouseEvent>(lastElement));
+			lastElement.setOnDragDone(new DragDoneHandler<MouseEvent>(lastElement));
+			((DragEvent)event).setDropCompleted(true);
+		}
+	}
+	
+	/**
+	 * Class for handling drag events from source node.
+	 * @param <T> the event type - should be DragEvent
+	 */
+	private class DragHandler<T extends Event> implements EventHandler<T> {
+
+		private Text text;
+		
+		public DragHandler(Text text) {
+			this.text = text;
+		}
+		
+		@Override
+		public void handle(Event event) {
+			Dragboard db = text.startDragAndDrop(TransferMode.MOVE);
+			ClipboardContent content = new ClipboardContent();
+			content.putString(text.getText());
+			db.setContent(content);
+			event.consume();
+		}
+	}
+	
+	/**
+	 * Deletes source element from list when drag and drop is successful.
+	 */
+	private class DragDoneHandler<T extends Event> implements EventHandler<DragEvent> {
+
+		private Text text;
+		
+		public DragDoneHandler(Text text) {
+			this.text = text;
+		}
+		
+		@Override
+		public void handle(DragEvent event) {
+			text.setText("");
+			((VBox)text.getParent()).getChildren().remove(text);
+		}
 	}
 	
 	@FXML
@@ -58,6 +156,10 @@ public class GTDController {
 		System.out.println("idea: " + idea + " list: " + list);
 		
 		Text text = new Text(idea);
+		
+		text.setOnDragDetected(new DragHandler<MouseEvent>(text));
+		
+		text.setOnDragDone(new DragDoneHandler<MouseEvent>(text));
 		
 		switch(list) {
 		case "in":
