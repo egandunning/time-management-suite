abc3b90d63bd78baff280323c21fc080f74788bb
egandunning <dunninea@plu.edu>
2017-12-31 17:44:48 -0500

    fixed formatting

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,25 +7,24 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
-	
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/main_window.fxml"));
-			primaryStage.setScene(new Scene(root, 800, 600));
-			primaryStage.setTitle("Time management suite");
-			primaryStage.getIcons().add(new Image("time-ms-icon.png"));
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-		//cancel timer on the pomodoro tab.
-		ui.controller.Pomodoro.cancelTimer();
-	}
+   
+   @Override
+   public void start(Stage primaryStage) {
+      try {
+         Parent root = FXMLLoader.load(getClass().getResource("/main_window.fxml"));
+         primaryStage.setScene(new Scene(root, 800, 600));
+         primaryStage.setTitle("Time management suite");
+         primaryStage.getIcons().add(new Image("time-ms-icon.png"));
+         primaryStage.show();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+
+   public static void main(String[] args) {
+      launch(args);
+      // cancel timer on the pomodoro tab.
+      ui.controller.Pomodoro.cancelTimer();
+   }
 }
