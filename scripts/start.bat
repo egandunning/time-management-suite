e7b067d22dcd804c6b1835594de9f211735d06c1
egandunning <dunninea@plu.edu>
2018-01-18 00:17:50 -0500

    added startup script for windows

---
+++ scripts/start.bat
@@ -0,0 +1,3 @@
+REM run jar with the filename "time-management-suite.jar"
+REM run with javaw so that the program still runs when the cmmand prompt closes
+START javaw -jar time-management-suite.jar
